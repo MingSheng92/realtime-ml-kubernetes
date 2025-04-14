d716bccc1216347ea549ba91fbb4dc1b0de8ad40
MingSheng92 <cming0721@gmail.com>
2025-04-14 03:32:10 +0000

    Adds precommit hook with ruff and tests the automation

--- services/trades/src/trades/config.py
+++ services/trades/src/trades/config.py
@@ -1,15 +1,25 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    # load from our env file 
+    # load from our env file
     model_config = SettingsConfigDict(
-        env_file='services/trades/settings.env',
-        env_file_encoding='utf-8'
+        env_file='services/trades/settings.env', env_file_encoding='utf-8'
     )
 
-    product_ids: list[str] = ['BTC/USD', 'BTC/EUR', 'ETH/USD', 'ETH/EUR', 'SOL/USD', 'SOL/EUR', 'XRP/USD', 'XRP/EUR']	
-    kafka_broker_address: str 
+    product_ids: list[str] = [
+        'BTC/USD',
+        'BTC/EUR',
+        'ETH/USD',
+        'ETH/EUR',
+        'SOL/USD',
+        'SOL/EUR',
+        'XRP/USD',
+        'XRP/EUR',
+    ]
+    kafka_broker_address: str
     kafka_topic_name: str
+
 
 config = Settings()
 # print(settings.model_dump())
